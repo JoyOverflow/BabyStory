b5a3ed42f2ed06575d20747f24213c5fb213f796
joyoverflow <joyoverflow@sina.com>
2019-06-03 00:19:02 +0800

    状态栏设置为沉浸模式

--- app/src/main/java/ouyj/hyena/com/babystory/BaseActivity.java
+++ app/src/main/java/ouyj/hyena/com/babystory/BaseActivity.java
@@ -1,7 +1,12 @@
 package ouyj.hyena.com.babystory;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -9,5 +14,48 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
+
+        //将状态栏设置为沉浸模式
+        setStatusBar();
+
+
+    }
+    /**
+     * 状态栏沉浸模式
+     */
+    private void setStatusBar() {
+
+        //当前设备是否大于等于Android5.0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            Window win= getWindow();
+
+            //禁止将状态和导航栏设置为透明色（注部分机型会有默认的背景色，并不是透明）
+            win.clearFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS|
+                            WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION
+            );
+            //win.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //win.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+            //开启全屏模式
+            win.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+
+            //只有先加入此Flag才能调用setStatusBarColor来设置状态栏颜色
+            win.addFlags(
+                    WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS
+            );
+            //为状态栏设置背景色（只能用于Android5.0及以上）
+            win.setStatusBarColor(
+                    ContextCompat.getColor(this, R.color.color_green)
+            );
+
+            getSupportActionBar().hide();
+        }
     }
 }
--- app/src/main/java/ouyj/hyena/com/babystory/StoriesActivity.java
+++ app/src/main/java/ouyj/hyena/com/babystory/StoriesActivity.java
@@ -1,13 +1,17 @@
 package ouyj.hyena.com.babystory;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StoriesActivity extends AppCompatActivity {
+public class StoriesActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stories);
+
+
+
+
+
     }
 }

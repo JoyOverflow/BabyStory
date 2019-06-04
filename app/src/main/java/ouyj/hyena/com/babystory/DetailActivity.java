06d0c179fdebad07f3d3e0e0598305610274bbc9
joyoverflow <joyoverflow@sina.com>
2019-06-04 22:31:14 +0800

    添加详情活动

--- app/src/main/java/ouyj/hyena/com/babystory/DetailActivity.java
+++ app/src/main/java/ouyj/hyena/com/babystory/DetailActivity.java
@@ -1,12 +1,42 @@
 package ouyj.hyena.com.babystory;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+/**
+ * 详细的一则故事页
+ */
 public class DetailActivity extends BaseActivity {
+
+    private String title;
+    private String content;
+
+    private TextView mTvText;
+    private ImageView mIvSelect;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        title = getIntent().getStringExtra("title");
+        content = getIntent().getStringExtra("content");
+        setTitle(title);
+
+        mTvText = findViewById(R.id.txt_content);
+        mTvText.setText(content);
+
+
+        //查找选择图像按钮
+        mIvSelect = findViewById(R.id.img_select);
+        mIvSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+
     }
 }
